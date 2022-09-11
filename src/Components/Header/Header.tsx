26d0a4b409dddfe70c8b341e7b94600c97008ac3
QuangNguyen <quangkl198@gmail.com>
2022-09-11 12:08:33 +0700

    assets

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,49 +1,130 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
+import {
+    Navbar,
+    MobileNav,
+    Typography,
+    Button,
+    IconButton,
+} from "@material-tailwind/react";
 
 const Header = () => {
-    const [navbar, setNavbar] = useState(false);
+    const [openNav, setOpenNav] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener(
+            "resize",
+            () => window.innerWidth >= 960 && setOpenNav(false),
+        );
+    }, []);
+
+    const navList = (
+        <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+            <Typography
+                as="li"
+                variant="small"
+                color="blue-gray"
+                className="p-1 font-normal"
+            >
+                <a href="#" className="flex items-center">
+                    Pages
+                </a>
+            </Typography>
+            <Typography
+                as="li"
+                variant="small"
+                color="blue-gray"
+                className="p-1 font-normal"
+            >
+                <a href="#" className="flex items-center">
+                    Account
+                </a>
+            </Typography>
+            <Typography
+                as="li"
+                variant="small"
+                color="blue-gray"
+                className="p-1 font-normal"
+            >
+                <a href="#" className="flex items-center">
+                    Blocks
+                </a>
+            </Typography>
+            <Typography
+                as="li"
+                variant="small"
+                color="blue-gray"
+                className="p-1 font-normal"
+            >
+                <a href="#" className="flex items-center">
+                    Docs
+                </a>
+            </Typography>
+        </ul>
+    );
 
     return (
-        <div>
-            <div className=" bg-green-600 border-b-4 border-solid border-yellow-400">
-                <div className="container mx-auto">
-                    <div className="flex justify-end px-3 py-4">
-                        <div className="">
-                            <a
-                                href=""
-                                className="px-4 border-r font-semibold text-gray-300 hover:text-white duration-100 text-lg"
-                            >
-                                Về T52
-                            </a>
-                            <a
-                                href=""
-                                className="px-4 border-r font-semibold text-gray-300 hover:text-white duration-100 text-lg"
-                            >
-                                Quan hệ Nhà đầu tư
-                            </a>
-                            <a
-                                href=""
-                                className="px-4 border-r font-semibold text-gray-300 hover:text-white duration-100 text-lg"
-                            >
-                                Tin tức
-                            </a>
-                            <a
-                                href=""
-                                className="px-4 border-r font-semibold text-gray-300 hover:text-white duration-100 text-lg"
-                            >
-                                Cửa hàng gần bạn
-                            </a>
-                            <a
-                                href=""
-                                className="px-4 font-semibold text-gray-300 hover:text-white duration-100 text-lg"
-                            >
-                                Kiến thức tài chính
-                            </a>
-                        </div>
-                    </div>
-                    <div></div>
-                </div>
+        <div className="mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4">
+            <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+                <Typography
+                    as="a"
+                    href="#"
+                    variant="small"
+                    className="mr-4 cursor-pointer py-1.5 font-normal"
+                >
+                    <span>Material Tailwind</span>
+                </Typography>
+                <div className="hidden lg:block">{navList}</div>
+                <Button
+                    variant="gradient"
+                    size="sm"
+                    className="hidden lg:inline-block text-gray-800"
+                >
+                    <span>Buy Now</span>
+                </Button>
+                <IconButton
+                    variant="text"
+                    className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+                    ripple={false}
+                    onClick={() => setOpenNav(!openNav)}
+                >
+                    {openNav ? (
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            className="h-6 w-6"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            strokeWidth={2}
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M6 18L18 6M6 6l12 12"
+                            />
+                        </svg>
+                    ) : (
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-6 w-6"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth={2}
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M4 6h16M4 12h16M4 18h16"
+                            />
+                        </svg>
+                    )}
+                </IconButton>
             </div>
+            <MobileNav open={openNav}>
+                {navList}
+                <Button variant="gradient" size="sm" fullWidth className="mb-2">
+                    <span>Buy Now</span>
+                </Button>
+            </MobileNav>
         </div>
     );
 };
