65637f47b730997b39eeb6a4d15f954bc754627b
HiepHoang2312 <hoangnhathiep0@gmail.com>
2022-09-15 11:49:21 +0700

    hkhkhh

--- src/Pages/StoresNearYou/StoresNearYou.tsx
+++ src/Pages/StoresNearYou/StoresNearYou.tsx
@@ -2,21 +2,12 @@ import Transaction from "Components/Transaction/Transaction";
 import React from "react";
 
 const StoresNearYou = () => {
-<<<<<<< HEAD
   return (
     <div>
       <div className="lg:h-40 h-24"></div>
       <Transaction />
     </div>
   );
-=======
-    return (
-        <div>
-            <div className="lg:h-28 h-20"></div>
-            <Transaction />
-        </div>
-    );
->>>>>>> 5217b1632b4ee6bbdfedbe48c63aa2c8324bd118
 };
 
 export default StoresNearYou;
