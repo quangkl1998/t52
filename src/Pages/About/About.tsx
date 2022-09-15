f2ce57e45f5b6d61d7c0ac7b22ff9e9dc5ed48bc
HiepHoang2312 <hoangnhathiep0@gmail.com>
2022-09-15 11:52:08 +0700

    aboutdone26

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -1,21 +1,20 @@
 import AboutT52 from "Components/AboutT52/AboutT52";
-import Advantage from "Components/Advantage/Advantage";
+
 import CoreValues from "Components/CoreValues/CoreValues";
-import FinancialService from "Components/FinancialService/FinancialService";
+
 import Future from "Components/Future/Future";
 import React from "react";
 
 const About = () => {
-    return (
-        <div className="bg-gray-100">
-            <div className="lg:h-36 h-20"></div>
-            <AboutT52 />
-            <CoreValues />
-            <Advantage />
-            <Future />
-            <FinancialService />
-        </div>
-    );
+  return (
+    <div className="bg-gray-100">
+      <div className="lg:h-36 h-20"></div>
+      <AboutT52 />
+      <CoreValues />
+
+      <Future />
+    </div>
+  );
 };
 
 export default About;
