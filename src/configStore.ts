2f7d13fad42d2e911ede498dfcf05aaa611dc494
HiepHoang2312 <hoangnhathiep0@gmail.com>
2022-10-26 03:09:50 +0700

    update[configStore]:update store

--- src/configStore.ts
+++ src/configStore.ts
@@ -4,15 +4,15 @@ import news from "./Slices/news";
 import banner from "./Slices/banner";
 import stores from "./Slices/store";
 import partner from "./Slices/partner";
+import media from "./Slices/media";
 
 import clientAdmin from "./Slices/ClientAdmin";
 import login from "./Slices/Login";
 import newsAdmin from "./Slices/NewsAdmin";
 import storeAdmin from "./Slices/StoreAdmin";
 import partnerAdmin from "./Slices/PartnerAdmin";
-<<<<<<< HEAD
-import tagNewsAdmin from "Slices/TagNewsAdmin";
-import mediaAdmin from "Slices/mediaAdmin";
+import tagNewsAdmin from "./Slices/TagNewsAdmin";
+import mediaAdmin from "./Slices/mediaAdmin";
 
 const store = configureStore({
   reducer: {
@@ -29,24 +29,6 @@ const store = configureStore({
     tagNewsAdmin,
     mediaAdmin,
   },
-=======
-import media from "./Slices/media";
-
-const store = configureStore({
-    reducer: {
-        auth,
-        news,
-        banner,
-        stores,
-        partner,
-        clientAdmin,
-        login,
-        newsAdmin,
-        storeAdmin,
-        partnerAdmin,
-        media,
-    },
->>>>>>> f54f4d206a03d33e9ee8b22b01747b253b79ca36
 });
 
 // type cho hàm dispatch
